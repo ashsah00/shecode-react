71946a21ff90a0fe891be923e0f03af0715de6cf
ashsah00 <ash.sahin.vz@gmail.com>
2024-08-01 22:18:10 -0400

    css
    css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import "./App.css";
 import Weather from "./Weather";
-import { DNA } from "react-loader-spinner";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <DNA
-          visible={true}
-          height="180"
-          width="180"
-          ariaLabel="dna-loading"
-          wrapperStyle={{}}
-          wrapperClass="dna-wrapper"
-        />
-        <p>
-          <Weather city="Paris" />
-        </p>
-      </header>
-      <footer>
-        Coded by <em>Ash Sahin.</em>
-      </footer>
+      <div className="container">
+        <body>
+          <Weather />
+        </body>
+        <footer>
+          Coded by <em>Ash Sahin.</em> Code hosted on{" "}
+          <a href="https://github.com/ashsah00/shecode-react">Github</a> and
+          website on <a href="https://weatheract.netlify.app">Netlify</a>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,25 +1,58 @@
 import React from "react";
-import axios from "axios";
+//import axios from "axios";
 import "./Weather.css";
 
-function Weather(props) {
-  function showTemp(response) {
-    console.log(response.data.main.temp);
-  }
-  let key = "25fad9f7e87157d33dde0f82ab269ee8";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${key}&units=metric`;
-  axios.get(url).then(showTemp);
-
+function Weather() {
   return (
-    <div>
-      Weather is great in {props.city}
-      <br />
-      <br />
-      <footer>
-        Coded by <em>Ash Sahin.</em>Code hosted on{" "}
-        <a href="https://github.com/ashsah00/shecode-react">Github</a> and
-        website on <a href="https://weatheract.netlify.app">Netlify</a>
-      </footer>
+    <div className="weatherContainer">
+      <form className="row">
+        <div class="col-9">
+          <input
+            type="Search"
+            placeholder="Enter A City..."
+            //  className="cityInput"
+            className="form-control"
+            autoFocus="on"
+          ></input>
+        </div>
+        <div class="col-3">
+          <input
+            type="submit"
+            text="Search"
+            //className="submit"
+            className="btn btn-primary w-100"
+          ></input>
+        </div>
+      </form>
+
+      <div className="formOutput">
+        <h1>New York</h1>
+
+        <div class="sub-body">
+          <ul className="day-time">
+            <li>Wednesday 08:02pm</li>
+            <li>Mostly sunny</li>
+          </ul>
+          <span className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="weather icon"
+            />
+            <span className="unitTemp">
+              <span class="Temp">60 </span>
+              <span class="Unit">ºC | ºF</span>
+            </span>
+          </span>
+
+          <div className="details">
+            <ul>
+              <li>Precipitation: 15%</li>
+              <li>Humidity: 72%</li>
+              <li>Wind: 5 m/h</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
